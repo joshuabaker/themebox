1cf235d9e4eebd096ec7958c649c21c2ba448959
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2022-01-28 11:29:57 +0000

    Default to p element type for Text

--- src/Text.jsx
+++ src/Text.jsx
@@ -5,6 +5,11 @@ import { Box } from "./Box";
 export const Text = forwardRef(function Text({ sx, ...props }, ref) {
   const { base, modifierStyles } = useThemeComponent("Text", props);
   return (
-    <Box ref={ref} sx={{ ...base, ...modifierStyles, ...sx }} {...props} />
+    <Box
+      ref={ref}
+      as={"p"}
+      sx={{ ...base, ...modifierStyles, ...sx }}
+      {...props}
+    />
   );
 });
