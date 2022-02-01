fb7bd4b662fcbee39286e89582ac44c47e0dfa00
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2022-02-01 19:27:00 +0000

    Use space key for Stack spacing

--- src/Stack.jsx
+++ src/Stack.jsx
@@ -1,18 +1,20 @@
 import { forwardRef } from "react";
 import { Flex } from "./Flex";
+import { useTheme } from "./useTheme";
 import { useThemeComponent } from "./useThemeComponent";
 
 export const Stack = forwardRef(function Stack(
   { spacing, spacingX = spacing, spacingY = spacing, sx, ...props },
   ref
 ) {
+  const { space } = useTheme();
   const { base, modifierStyles } = useThemeComponent("Stack", props);
   return (
     <Flex
       ref={ref}
       sx={{
-        "--spacing-x": spacingX,
-        "--spacing-y": spacingY,
+        "--spacing-x": space?.[spacingX] ?? spacingX,
+        "--spacing-y": space?.[spacingY] ?? spacingY,
         mx: "calc(var(--spacing-x) * -1)",
         my: "calc(var(--spacing-y) * -1)",
         "& > *:not(style)": {
